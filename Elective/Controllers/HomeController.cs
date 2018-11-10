4f43b2bc5e5cca7317c5d4833e59397a3796428f
Дмитрий Водолажский <un1eashed401@gmail.com>
2018-11-10 14:48:03 +0200

    2nd commit

--- Elective/Controllers/HomeController.cs
+++ Elective/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,32 +10,43 @@ namespace Elective.Controllers
 {
     public class HomeController : Controller
     {
+        List<Student> students = new List<Student>();
+        List<Subject> subjects = new List<Subject>();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult Students()
         {
-            List<Student> students = new List<Student>();
             students.Add(new Student(1,"Igor","Grohotsky"));
             students.Add(new Student(2,"Dmitriy","Vodolazhskiy"));
-            students.Add(new Student(2,"Alexander","Chekmarev"));
+            students.Add(new Student(3,"Alexander","Chekmarev"));
             return View(students);
+        }
+        
+        public ActionResult Subjects()
+        {
+            subjects.Add(new Subject(1, "ASP.NET", new Instructor(1, "Alex", "Bazhenov"), "Programming", Subject.Status.started));
+            subjects.Add(new Subject(2, "Java", new Instructor(2, "Oleg", "Hromov"), "Programming", Subject.Status.processing));
+            return View(subjects);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            students.Add(new Student(1, "Igor", "Grohotsky"));
+            students.Add(new Student(2, "Dmitriy", "Vodolazhskiy"));
+            students.Add(new Student(3, "Alexander", "Chekmarev"));
+            Student student = students.SingleOrDefault(e => e.Id == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
         }
     }
 }
--- Elective/Models/Subject.cs
+++ Elective/Models/Subject.cs
@@ -8,16 +8,31 @@ namespace Elective.Models
     //information about the subject
     public class Subject
     {
+        public enum Status
+        {
+            started = 1,
+            processing = 2,
+            finished = 3
+        }
+        public Status Course_status { get; set; }
         public byte Id { get; set; }
         public string Subject_name { get; set; }
         public Instructor Instructor { get; set; }
         public string Topic { get; set; }
         private List<Student> Students = new List<Student>();
-        public bool IsStarted { get; set; }
 
         public void Add(Student student)
         {
             Students.Add(student);
+        }
+
+        public Subject(byte id, string course_name, Instructor instructor, string topic, Status status)
+        {
+            Id = id;
+            Subject_name = course_name;
+            Instructor = instructor;
+            Topic = topic;
+            Course_status = status;
         }
 
     }
